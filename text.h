c09889dde3ebe9e73375507c3316ca1bb75af3c1
AIDDQD <artemiy.bezgin@gmail.com>
2022-04-03 23:26:32 +0300

    Add Move* and ShowInsertPosition functions to TextDisplay

--- text.h
+++ text.h
@@ -12,6 +12,11 @@ extern "C" {
   extern GText_Display *go_fltk_new_TextDisplay(int x, int y, int w, int h, const char *text);
   extern void go_fltk_TextDisplay_set_buffer(GText_Display *d, Fl_Text_Buffer *buf);
   extern void go_fltk_TextDisplay_set_wrap_mode(GText_Display *b, int wrap, int wrapMargin);
+  extern int go_fltk_TextDisplay_move_right(GText_Display *b);
+  extern int go_fltk_TextDisplay_move_left(GText_Display *b);
+  extern int go_fltk_TextDisplay_move_up(GText_Display *b);
+  extern int go_fltk_TextDisplay_move_down(GText_Display *b);
+  extern void go_fltk_TextDisplay_show_insert_position(GText_Display *b);
   extern Fl_Text_Buffer *go_fltk_TextDisplay_buffer(GText_Display *d);
   extern Fl_Text_Buffer *go_fltk_new_TextBuffer(void);
   extern void go_fltk_TextBuffer_set_text(Fl_Text_Buffer *b, const char *txt);
