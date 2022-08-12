cc65c554e13f0668701c36f0b37db7ab9f8d9787
Mohammed Alyousef <may642_2000@hotmail.com>
2022-08-12 23:26:11 +0300

    add window fullscreen methods

--- window.go
+++ window.go
@@ -36,6 +36,18 @@ func (w *Window) SetCursor(cursor Cursor) {
 	C.go_fltk_Window_set_cursor((*C.GWindow)(w.ptr()), C.int(cursor))
 }
 
+func (w *Window) SetFullscreen(flag bool) {
+	f := 0
+	if flag {
+		f = 1
+	}
+	C.go_fltk_Window_set_fullscreen((*C.GWindow)(w.ptr()), C.int(f))
+}
+
+func (w *Window) FullscreenActive() bool {
+	return C.go_fltk_Window_fullscreen_active((*C.GWindow)(w.ptr())) != 0
+}
+
 type Cursor int
 
 var (
