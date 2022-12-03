02a5d933ac5828538fe01bb8d9c057f7d732096e
Steve Kieu <msh.computing@gmail.com>
2022-12-03 11:26:53 +1000

    Add lots of text buffer and editor functions
    These allow us to have more control over the editor.

--- text.go
+++ text.go
@@ -48,6 +48,122 @@ func (b *TextBuffer) Text() string {
 	return C.GoString(C.go_fltk_TextBuffer_text(b.ptr()))
 }
 
+// GetTextRange - get text from start and end position
+func (b *TextBuffer) GetTextRange(start, end int) string {
+	return C.GoString(C.go_fltk_TextBuffer_text_range(b.ptr(), C.int(start), C.int(end)))
+}
+
+// Highlight - highlight text from start and end position
+func (b *TextBuffer) Highlight(start, end int) {
+	C.go_fltk_TextBuffer_highlight(b.ptr(), C.int(start), C.int(end))
+}
+
+// UnHighlight all highlighted text
+func (b *TextBuffer) UnHighlight() {
+	C.go_fltk_TextBuffer_unhighlight(b.ptr())
+}
+
+// ReplaceRange - replace text within the start, end range with text
+func (b *TextBuffer) ReplaceRange(start, end int, txt string) {
+	txtstr := C.CString(txt)
+	defer C.free(unsafe.Pointer(txtstr))
+	C.go_fltk_TextBuffer_replace(b.ptr(), C.int(start), C.int(end), txtstr)
+}
+
+// ReplaceSelection - replace text within current selection with text
+func (b *TextBuffer) ReplaceSelection(txt string) {
+	txtstr := C.CString(txt)
+	defer C.free(unsafe.Pointer(txtstr))
+	C.go_fltk_TextBuffer_replace_selection(b.ptr(), txtstr)
+}
+
+// SearchForward - search forward for searchStr from position start. return the position if found otherwise -1
+func (b *TextBuffer) SearchForward(start int, searchStr string, matchCase bool) (foundPos int) {
+	txtstr := C.CString(searchStr)
+	defer C.free(unsafe.Pointer(txtstr))
+	_matchCase := 1
+	if !matchCase {
+		_matchCase = 0
+	}
+	var C_foundPos C.int
+	_isFound := C.go_fltk_TextBuffer_search_forward(b.ptr(), C.int(start), txtstr, &C_foundPos, C.int(_matchCase))
+	if int(_isFound) == 0 {
+		foundPos = -1
+	} else {
+		foundPos = int(C_foundPos)
+	}
+	return foundPos
+}
+
+// SearchBackward - search backward for searchStr from position start. return the position if found otherwise -1
+func (b *TextBuffer) SearchBackward(start int, searchStr string, matchCase bool) (foundPos int) {
+	txtstr := C.CString(searchStr)
+	defer C.free(unsafe.Pointer(txtstr))
+	_matchCase := 1
+	if !matchCase {
+		_matchCase = 0
+	}
+	var C_foundPos C.int
+	_isFound := C.go_fltk_TextBuffer_search_backward(b.ptr(), C.int(start), txtstr, &C_foundPos, C.int(_matchCase))
+	if int(_isFound) == 0 {
+		foundPos = -1
+	} else {
+		foundPos = int(C_foundPos)
+	}
+	return foundPos
+}
+
+// Search - search text in the buffer for string searchStr. Return the position found or -1
+func (b *TextBuffer) Search(start int, searchStr string, searchBackward, matchCase bool) (foundPos int) {
+	if searchBackward {
+		return b.SearchBackward(start, searchStr, matchCase)
+	} else {
+		return b.SearchForward(start, searchStr, matchCase)
+	}
+}
+
+// Select text between start and end
+func (b *TextBuffer) Select(start, end int) {
+	C.go_fltk_TextBuffer_select(b.ptr(), C.int(start), C.int(end))
+}
+
+// Selected Check if any text is selected
+func (b *TextBuffer) IsSelected(start, end int) bool {
+	selected := C.go_fltk_TextBuffer_selected(b.ptr())
+	if int(selected) == 0 {
+		return false
+	}
+	return true
+}
+
+// GetSelectionPosition - Get position (start, end) of the currently selected text
+func (b *TextBuffer) GetSelectionPosition() (int, int) {
+	var _start, _end C.int
+	C.go_fltk_TextBuffer_selection_position(b.ptr(), &_start, &_end)
+	return int(_start), int(_end)
+}
+
+// GetSelectionText return the text within the current selection
+func (b *TextBuffer) GetSelectionText() string {
+	txtstr := C.go_fltk_TextBuffer_selection_text(b.ptr())
+	return C.GoString(txtstr)
+}
+
+// UnSelect - unselect any selections in the buffer
+func (b *TextBuffer) UnSelect() {
+	C.go_fltk_TextBuffer_unselect(b.ptr())
+}
+
+// SetTabWidth - set the TAB distance (width)
+func (b *TextBuffer) SetTabWidth(tabWidth int) {
+	C.go_fltk_TextBuffer_set_tab_distance(b.ptr(), C.int(tabWidth))
+}
+
+func (b *TextBuffer) GetTabWidth() int {
+	w := C.go_fltk_TextBuffer_tab_distance(b.ptr())
+	return int(w)
+}
+
 type TextDisplay struct {
 	widget
 }
@@ -112,13 +228,58 @@ func (t *TextDisplay) SetTextSize(size int) {
 	C.go_fltk_TextDisplay_set_text_size((*C.GText_Display)(t.ptr()), C.int(size))
 }
 
+// SetInsertPosition set the insert position to a new position
+func (t *TextDisplay) SetInsertPosition(newPos int) {
+	C.go_fltk_TextDisplay_insert_position((*C.GText_Display)(t.ptr()), C.int(newPos))
+}
+
+// GetInsertPosition - return the current insert position
+func (t *TextDisplay) GetInsertPosition() int {
+	return (int)(C.go_fltk_TextDisplay_get_insert_position((*C.GText_Display)(t.ptr())))
+}
+
+// InsertText - Insert text at the cursor position
+func (t *TextDisplay) InsertText(txt string) {
+	txtstr := C.CString(txt)
+	defer C.free(unsafe.Pointer(txtstr))
+	C.go_fltk_TextDisplay_insert_text((*C.GText_Display)(t.ptr()), txtstr)
+}
+
+// Overstrike - Not sure what it does, the fltk doc does not match with the name meaning
+func (t *TextDisplay) Overstrike(txt string) {
+	txtstr := C.CString(txt)
+	defer C.free(unsafe.Pointer(txtstr))
+	C.go_fltk_TextDisplay_overstrike((*C.GText_Display)(t.ptr()), txtstr)
+}
+
 type TextEditor struct {
 	TextDisplay
 }
 
-// Copy Does a copy of selected text or the current character in the current buffer of editor 'e'. (kf_copy)
+// Copy copy of selected text or the current character in the current buffer of editor 'e'. (kf_copy)
 func (t *TextEditor) Copy() {
 	C.go_fltk_TextEditor_copy((*C.GText_Editor)(t.ptr()))
+}
+
+// Insert - Togglesthe insert mode (kf_insert)
+func (t *TextEditor) Insert() {
+	C.go_fltk_TextEditor_insert((*C.GText_Editor)(t.ptr()))
+}
+
+func (t *TextEditor) Cut() {
+	C.go_fltk_TextEditor_cut((*C.GText_Editor)(t.ptr()))
+}
+
+func (t *TextEditor) Delete() {
+	C.go_fltk_TextEditor_delete((*C.GText_Editor)(t.ptr()))
+}
+
+func (t *TextEditor) Paste() {
+	C.go_fltk_TextEditor_paste((*C.GText_Editor)(t.ptr()))
+}
+
+func (t *TextEditor) SelectAll() {
+	C.go_fltk_TextEditor_select_all((*C.GText_Editor)(t.ptr()))
 }
 
 func NewTextEditor(x, y, w, h int, text ...string) *TextEditor {
