184f80fb8cf01a1a7084f7ce18e907f42282a43b
Piotr Wieczorek <p@wie.cz>
2023-03-30 20:43:40 +0200

    Use extern keywork consistently

--- text.h
+++ text.h
@@ -30,33 +30,34 @@ extern "C" {
   extern int go_fltk_TextDisplay_text_size(Fl_Text_Display *d);
   extern void go_fltk_TextDisplay_set_text_size(Fl_Text_Display *d, int size);
   extern Fl_Text_Buffer *go_fltk_TextDisplay_buffer(Fl_Text_Display *d);
-  void go_fltk_TextDisplay_insert_position(Fl_Text_Display *d, int newPos);
-  int go_fltk_TextDisplay_get_insert_position(Fl_Text_Display *d);
-  void go_fltk_TextDisplay_insert_text(Fl_Text_Display *d, const char *text);
-  void go_fltk_TextDisplay_overstrike(Fl_Text_Display *d, const char* text);
+  extern void go_fltk_TextDisplay_insert_position(Fl_Text_Display *d, int newPos);
+  extern int go_fltk_TextDisplay_get_insert_position(Fl_Text_Display *d);
+  extern void go_fltk_TextDisplay_insert_text(Fl_Text_Display *d, const char *text);
+  extern void go_fltk_TextDisplay_overstrike(Fl_Text_Display *d, const char* text);
 
   extern Fl_Text_Buffer *go_fltk_new_TextBuffer(void);
   extern void go_fltk_TextBuffer_delete(Fl_Text_Buffer* b);
   extern void go_fltk_TextBuffer_set_text(Fl_Text_Buffer *b, const char *txt);
   extern void go_fltk_TextBuffer_append(Fl_Text_Buffer *b, const char *txt);
   extern const char *go_fltk_TextBuffer_text(Fl_Text_Buffer *b);
-  const char *go_fltk_TextBuffer_text_range(Fl_Text_Buffer *b, int start, int end);
-  void go_fltk_TextBuffer_highlight(Fl_Text_Buffer *b, int start, int end);
-  void go_fltk_TextBuffer_unhighlight(Fl_Text_Buffer *b);
-  void go_fltk_TextBuffer_replace(Fl_Text_Buffer *b, int start, int end, const char *text);
-  void go_fltk_TextBuffer_replace_selection(Fl_Text_Buffer *b, const char *text);
-  int go_fltk_TextBuffer_search_forward(Fl_Text_Buffer *b, int start, const char *searchString, int *foundPos, int matchCase);
-  int go_fltk_TextBuffer_search_backward(Fl_Text_Buffer *b, int start, const char *searchString, int *foundPos, int matchCase);
-  void go_fltk_TextBuffer_select(Fl_Text_Buffer *b, int start, int end);
-  int go_fltk_TextBuffer_selected(Fl_Text_Buffer *b);
-  int go_fltk_TextBuffer_selection_position(Fl_Text_Buffer *b, int *start, int *end);
-  char* go_fltk_TextBuffer_selection_text(Fl_Text_Buffer *b);
-  void go_fltk_TextBuffer_unselect(Fl_Text_Buffer *b);
-  int go_fltk_TextBuffer_tab_distance(Fl_Text_Buffer *b);
-  void go_fltk_TextBuffer_set_tab_distance(Fl_Text_Buffer *b, int tabDist);
-  extern void go_fltk_TextDisplay_set_highlight_data(Fl_Text_Display *self, Fl_Text_Buffer *sbuff, unsigned int *color, int *font, 
-                                     int *fontsz, unsigned *attr, unsigned int *bgcolor, int sz);                                                                                              \
-
+  extern const char *go_fltk_TextBuffer_text_range(Fl_Text_Buffer *b, int start, int end);
+  extern void go_fltk_TextBuffer_highlight(Fl_Text_Buffer *b, int start, int end);
+  extern void go_fltk_TextBuffer_unhighlight(Fl_Text_Buffer *b);
+  extern void go_fltk_TextBuffer_replace(Fl_Text_Buffer *b, int start, int end, const char *text);
+  extern void go_fltk_TextBuffer_replace_selection(Fl_Text_Buffer *b, const char *text);
+  extern int go_fltk_TextBuffer_search_forward(Fl_Text_Buffer *b, int start, const char *searchString, int *foundPos, int matchCase);
+  extern int go_fltk_TextBuffer_search_backward(Fl_Text_Buffer *b, int start, const char *searchString, int *foundPos, int matchCase);
+  extern void go_fltk_TextBuffer_select(Fl_Text_Buffer *b, int start, int end);
+  extern int go_fltk_TextBuffer_selected(Fl_Text_Buffer *b);
+  extern int go_fltk_TextBuffer_selection_position(Fl_Text_Buffer *b, int *start, int *end);
+  extern char* go_fltk_TextBuffer_selection_text(Fl_Text_Buffer *b);
+  extern void go_fltk_TextBuffer_unselect(Fl_Text_Buffer *b);
+  extern int go_fltk_TextBuffer_tab_distance(Fl_Text_Buffer *b);
+  extern void go_fltk_TextBuffer_set_tab_distance(Fl_Text_Buffer *b, int tabDist);
+  extern void go_fltk_TextDisplay_set_highlight_data(
+      Fl_Text_Display *self, Fl_Text_Buffer *sbuff, unsigned int *color,
+      int *font, int *fontsz, unsigned *attr, unsigned int *bgcolor, int sz);
+  
 #ifdef __cplusplus
 }
 #endif
