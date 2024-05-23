abe77d6d4a5dd988799aff1908d3f4ad940834f3
Robert Lillack <rob@lillack.net>
2024-05-23 08:02:53 +0200

    Add OpenBSD/amd64 support.

---
+++ cgo_openbsd_amd64.go
@@ -0,0 +1,8 @@
+//go:build openbsd && amd64
+
+package fltk
+
+// #cgo openbsd,amd64 CXXFLAGS: -std=c++11
+// #cgo openbsd,amd64 CPPFLAGS: -I${SRCDIR}/lib/openbsd/amd64 -I/usr/X11R6/include -I${SRCDIR}/include -I${SRCDIR}/include/FL/images -I${SRCDIR}/include/png -I${SRCDIR}/include/zlib -I${SRCDIR}/include/jpeg -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_THREAD_SAFE -D_REENTRANT
+// #cgo openbsd,amd64 LDFLAGS: -L/usr/X11R6/lib ${SRCDIR}/lib/openbsd/amd64/libfltk_images.a ${SRCDIR}/lib/openbsd/amd64/libfltk_jpeg.a ${SRCDIR}/lib/openbsd/amd64/libfltk_png.a ${SRCDIR}/lib/openbsd/amd64/libfltk_z.a ${SRCDIR}/lib/openbsd/amd64/libfltk_gl.a -lGLU -lGL ${SRCDIR}/lib/openbsd/amd64/libfltk_forms.a ${SRCDIR}/lib/openbsd/amd64/libfltk.a -lm -lX11 -lXext -lpthread -lXinerama -lXfixes -lXcursor -lXft -lXrender -lm -lfontconfig
+import "C"
